64792d795dafe6df751bb6025cc341fb91eee452
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-03-24 10:39:07 +0000

    Display sequence and proceed if correct number is pressed

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 function love.load()
-
+    sequence = {4, 3, 1, 2, 2, 4}
+    current = 1
 end
 
 function love.update(dt)
@@ -7,11 +8,19 @@ function love.update(dt)
 end
 
 function love.draw()
-
+    love.graphics.print(table.concat(sequence, ', '))
+    love.graphics.print(current..'/'..#sequence, 0, 20)
+    love.graphics.print('sequence[current]: '..sequence[current], 0, 40)
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
+    end
+    if tonumber(key) == sequence[current] then
+        current = current + 1
+        if current > #sequence then
+            current = 1
+        end
     end
 end

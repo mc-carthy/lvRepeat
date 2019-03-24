2e7ca680a3a265a0e4a7f899489875b597d56ff1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-03-24 10:58:53 +0000

    Create watch and repeat states, in watch state, sequence numbers are highlighted in turn

--- main.lua
+++ main.lua
@@ -3,45 +3,64 @@ function addToSequence()
 end
 
 function love.load()
-    sequence = {}
+    sequence = {4, 1, 3, 2, 2, 4}
     addToSequence()
     current = 1
+    timer = 0
+    state = 'watch'
 end
 
 function love.update(dt)
-
+    if state == 'watch' then
+        timer = timer + dt
+        if timer > 1 then
+            timer = 0
+            current = current + 1
+            if current > #sequence then
+                state = 'repeat'
+                current = 1
+            end
+        end
+    end
 end
 
 function love.draw()
-    local function drawSquare(number, color)
+    local function drawSquare(number, colour, highlightedColour)
         local squareSize = 50
-        love.graphics.setColor(color)
+        if state == 'watch' and number == sequence[current] then
+            love.graphics.setColor(highlightedColour)
+        else
+            love.graphics.setColor(colour)
+        end
         love.graphics.rectangle('fill', squareSize * (number - 1), 0, squareSize, squareSize)
         love.graphics.setColor(1, 1, 1)        
         love.graphics.print(number, squareSize * (number - 1) + 19, 14)
     end
     
-    drawSquare(1, { 0.2, 0, 0 })
-    drawSquare(2, { 0, 0.2, 0 })
-    drawSquare(3, { 0, 0, 0.2 })
-    drawSquare(4, { 0.2, 0.2, 0 })
+    drawSquare(1, { 0.2, 0, 0 }, { 1, 0, 0 })
+    drawSquare(2, { 0, 0.2, 0 }, { 0, 1, 0 })
+    drawSquare(3, { 0, 0, 0.2 }, { 0, 0, 1 })
+    drawSquare(4, { 0.2, 0.2, 0 }, { 1, 1, 0 })
 
-    love.graphics.print(current..'/'..#sequence, 20, 60)
-    love.graphics.print('sequence[current]: '..sequence[current], 20, 100)
+    love.graphics.print(current.. '/' ..#sequence, 20, 60)
+    love.graphics.print('sequence[current]: ' .. sequence[current], 20, 100)
     love.graphics.print(table.concat(sequence, ', '), 20, 140)
+    love.graphics.print('state: ' .. state, 20, 180)
 end
 
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
-    if tonumber(key) == sequence[current] then
-        current = current + 1
-        if current > #sequence then
-            current = 1
-            addToSequence()
+    if state == 'repeat' then
+        if tonumber(key) == sequence[current] then
+            current = current + 1
+            if current > #sequence then
+                current = 1
+                addToSequence()
+            end
+        else
+            love.load()
         end
-    else
-        love.load()
     end
 end
